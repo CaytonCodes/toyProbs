3d06e59a9eade6ee5d9e75db5999cf0f17a501df
Scotty Cayton <kcayton34@gmail.com>
2021-03-11 21:55:22 -0700

    Completed insert method for linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -23,6 +23,7 @@ class LinkedList {
   }
 
   // inserts a provided node at specified position (head, tail, or index)
+  // if index is entered, inserts at provided index
   insert(value, index = 'tail') {
     // handle edge case of bad index
     if (index > this.length) { return null; }
@@ -37,23 +38,28 @@ class LinkedList {
       if (this.tail === null) {
         this.tail = node;
       }
-    }
-
-    if (index === 'tail' || index === this.length) {
-      this.tail.next = node;
+    } else if (index === 'tail' || index === this.length) {
+      if (this.tail) {
+        this.tail.next = node;
+      }
       this.tail = node;
 
       // handle an empty list
       if (this.head === null) {
         this.head = node;
       }
+    } else {
+      const precedingNode = this.findNode(index - 1);
+
+      node.next = precedingNode.next;
+      precedingNode.next = node;
     }
 
     this.length += 1;
     return node;
   }
 
-  findIndex(index) {
+  findNode(index) {
     let currentNode = this.head;
     for (let i = 1; i <= index; i += 1) {
       if (currentNode === null) {
@@ -68,3 +74,14 @@ class LinkedList {
 module.exports = {
   LinkedList,
 };
+
+let list = new LinkedList();
+list.insert(0);
+list.insert(1);
+list.insert(2);
+list.insert(3);
+list.insert(4);
+list.insert(5, 5);
+console.log(list.length);
+console.log(list.findNode(0));
+console.log(list.findNode(8));
